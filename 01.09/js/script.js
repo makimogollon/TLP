ae626572788819347ba6abc20930efdef89dd87c
maki <mmogollon@usm.cl>
2025-09-01 15:47:21 -0400

    Add files via upload

---
+++ 01.09/js/script.js
@@ -0,0 +1,8 @@
+function saludar(){
+
+    //document.write("hola desde funcion")
+
+    var titulo = document.getElementById("saludo")
+    titulo.classList.add("rojo")
+    titulo.innerHTML = "hola desde funcion"
+}
